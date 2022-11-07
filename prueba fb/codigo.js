46d9ad2ac226bd12544b66754e37672b28c0df3c
LeoRodriguez02 <115320268+LeoRodriguez02@users.noreply.github.com>
2022-11-07 14:37:59 -0300

    prueba

---
+++ prueba fb/codigo.js
@@ -0,0 +1,18 @@
+const nombre = document.getElementById("nombre")
+const mail = document.getElementById("mail")
+const materia = document.getElementById("contraseña")
+const boton = document.getElementById("btn-enviar")
+
+boton.addEventListener("click",(e)=>{
+	console.log(nombre.value);
+});
+
+
+
+
+
+
+
+
+
+
