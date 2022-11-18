6a6d9440fa0766fe7ef74488826f14b21a1f272a
Leandro Tapia <leandro.tapia43@gmail.com>
2022-11-18 01:11:36 -0300

    Add files via upload

--- Proyecto/formulario.js
+++ Proyecto/formulario.js
@@ -1,16 +1,35 @@
-const primeraOpcion = document.querySelector("#opcion_1");
-// const segunda_opcion = document.getElementById("opcion_2");
-// const tercera_opcion = document.getElementById("opcion_3");
+const primeraOpcion = document.getElementById('opcion_1');
+const segundaOpcion = document.getElementById("opcion_2");
+const terceraOpcion = document.getElementById("opcion_3");
+const sendForm = document.querySelector('.enviar');
 
-primeraOpcion.addEventListener("click",(e)=>{
-    console.log("1")
-})
-// segunda_opcion.addEventListener("Click",(e)=>{
-// 	console.log("1");
-// })
+if (1 < 3) {
+    primeraOpcion.addEventListener('click',(e)=>{
+        e.preventDefault();
+        console.log('1');
+    });
+}
 
-// tercera_opcion.addEventListener("Click",(e)=>{
-// 	console.log("1");
-// })
+if (1 < 3) {
+    segundaOpcion.addEventListener('click',(e)=>{
+        e.preventDefault();
+        console.log('2');
+    });
+}
 
-console.log("hola")
+if (1 < 3) {
+    terceraOpcion.addEventListener('click',(e)=>{
+        e.preventDefault();
+        console.log('3');
+    });
+}
+
+if (1 < 3) {
+    sendForm.addEventListener('click',(e)=>{
+        e.preventDefault();
+        console.log('incendio reportado');
+        alert('Reportado exitosamente');
+        window.open('https://youtube.com')
+    });
+}
+
