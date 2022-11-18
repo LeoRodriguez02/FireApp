2bbee095f87e59ba1fe280f5b7c742b9184cc16d
Leandro Tapia <leandro.tapia43@gmail.com>
2022-11-18 00:31:38 -0300

    Add files via upload
    mio

---
+++ Proyecto/formulario.js
@@ -0,0 +1,16 @@
+const primeraOpcion = document.querySelector("#opcion_1");
+// const segunda_opcion = document.getElementById("opcion_2");
+// const tercera_opcion = document.getElementById("opcion_3");
+
+primeraOpcion.addEventListener("click",(e)=>{
+    console.log("1")
+})
+// segunda_opcion.addEventListener("Click",(e)=>{
+// 	console.log("1");
+// })
+
+// tercera_opcion.addEventListener("Click",(e)=>{
+// 	console.log("1");
+// })
+
+console.log("hola")
